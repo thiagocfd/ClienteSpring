d557476d0869b75200febbe6dcc18b2ba65ba5d8
Thiago Dourado <thiagocfdourado@gmail.com>
2022-02-08 16:55:12 -0300

    Configuração do formulário de serviços prestados

--- src/main/java/io/github/thiagocfd/clientes/rest/ServicoPrestadoController.java
+++ src/main/java/io/github/thiagocfd/clientes/rest/ServicoPrestadoController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/servicos-prestados")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class ServicoPrestadoController {
 
     private final ClienteRepository clienteRepository;
@@ -26,7 +28,7 @@ public class ServicoPrestadoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ServicoPrestado salvar(@RequestBody ServicoPrestadoDTO dto) {
+    public ServicoPrestado salvar(@RequestBody @Valid ServicoPrestadoDTO dto) {
         LocalDate data = LocalDate.parse(dto.getData(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
         Integer idCliente = dto.getIdCliente();
 
